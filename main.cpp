67047e3226bc6cf212a471bc3477672a26ea29d5
cuongpro97 <boydavion@gmail.com>
2016-11-16 17:31:57 +0700

    Commit

--- main.cpp
+++ main.cpp
@@ -26,12 +26,48 @@ int hamso(int a, int b, int c, int d, int e){
 }
 
 //todo: viet ham in ra tu 1->10 dung do...while
-
+void lap_doWhile(int n, int i)
+{
+    printf("nhap vao gia tri n: ");
+    scanf("%d",&n);
+    do
+        printf("%d\n",i);
+    while(i++,i<=n);
+}
 //todo: viet ham in ra tu 1->10 dung while
-
+void lap_while(int n, int i)
+{
+    printf("nhap vao gia tri n: ");
+    scanf("%d",&n);
+    while(i++,i<=n)
+        printf("%d\n",i);
+}
 //todo: viet ham in ra tu 1->10 dung for
-
+void lap_for(int n, int i)
+{
+    printf("nhap vao gia tri n: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        printf("%d\n",i);
+}
 //todo: viet ham tinh tong so tu 1->10, vi du: 1+2+....+10.
+void tong_1denN(int n,int i,int s)
+{
+    printf("nhap vao gia tri n: ");
+    scanf("%d",&n);
+    while(i++,i<=n)
+        s=s+i;
+    printf("%d",s);
+}
+//main cho tat ca
+int main()
+{
+    int n,i,j=0,k=1,s=0;
+    lap_for(n,i);
+    lap_while(n,j);
+    lap_doWhile(n,k);
+    tong_1denN(n,j,s);
+}
 
 
 int main() {
